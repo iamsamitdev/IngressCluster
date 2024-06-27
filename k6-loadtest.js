33b9d0a7356eae22c6a840d4200e6b2b74f33add
Samit Koyom <samitkoyom@gmail.com>
2024-06-27 13:51:25 +0700

    add hpa v2

--- k6-loadtest.js
+++ k6-loadtest.js
@@ -1,7 +1,16 @@
-import http from 'k6/http'
-import { sleep } from 'k6'
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '10s', target: 200 }, // ramp up to 10 VUs
+    { duration: '60s', target: 400 }, // stay at 20 VUs
+    { duration: '30s', target: 200 },  // ramp down to 0 VUs
+    { duration: '10s', target: 0 },  // ramp down to 0 VUs
+  ],
+};
 
 export default function () {
-  http.get('http://localhost:56016')
-  sleep(1)
+  http.get('http://localhost:53402');
+  sleep(1);
 }
